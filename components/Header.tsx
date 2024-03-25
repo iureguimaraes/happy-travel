dc255df08a93832389371466982ae6d17808d106
Iure Guimaraes <iurevguimaraes@gmail.com>
2024-03-24 22:17:39 -0300

    change header image behavior by city

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,14 +2,18 @@
 
 import Image from "next/image";
 
-export default function Header() {
+export default function Header({ city }: any) {
   return (
     <>
       <header className="relative w-full min-h-96 md:h-3/6 flex md:block flex-col jutify-center items-end">
         <Image
           priority
           fill
-          src="/assets/header-bg.jpg"
+          src={
+            city
+              ? `https://source.unsplash.com/random/1200x400/?${city}`
+              : "/assets/header-bg.jpg"
+          }
           alt="Sardenha"
           quality={100}
           className="pointer-events-none select-none"
