e641d04f213f72fb8bc3be2f7d6ad4a9d8997ede
Iure Guimaraes <iurevguimaraes@gmail.com>
2024-03-25 23:29:08 -0300

    populate homepage with components

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,19 @@
+import Grid from "@/components/Grid";
 import Header from "@/components/Header";
 
-export default function Home() {
+import { promises as fs } from "fs";
+
+export default async function Home() {
+  const file = await fs.readFile(process.cwd() + "/app/data.json", "utf8");
+  const data = JSON.parse(file);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between">
+    <main className="flex min-h-screen flex-col items-center justify-normal">
       <Header />
+
+      <h2>Teste</h2>
+
+      <Grid citiesData={data} />
     </main>
   );
 }
