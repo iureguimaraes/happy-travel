80fc5fba3bba35e63892d084aa94d808f3bc99d8
Iure Guimaraes <iurevguimaraes@gmail.com>
2024-03-25 23:28:29 -0300

    add grid component

---
+++ components/Grid/index.tsx
@@ -0,0 +1,56 @@
+"use client";
+import { promises as fs } from "fs";
+import { useEffect, useState } from "react";
+import Card from "../Card";
+
+interface dataType {
+  city: String;
+  content: String[];
+}
+
+export default function Grid({ citiesData }: any) {
+  const [data, setData] = useState<any[]>([]);
+  const [searchQuery, setSearchQuery] = useState("");
+
+  useEffect(() => {
+    setData(citiesData);
+  }, []);
+
+  async function getData() {
+    const file = await fs.readFile(process.cwd() + "/app/data.json", "utf8");
+    setData(JSON.parse(file));
+  }
+
+  return (
+    <>
+      <div className="flex w-96 rounded bg-white">
+        <input
+          type="text"
+          placeholder="Search by continent"
+          // className="mb-2 px-2 py-2 border-rounded"
+          onChange={(e) => setSearchQuery(e.target.value)}
+          className="w-full border-none bg-transparent px-4 py-1 text-gray-900 outline-none focus:outline-none"
+        />
+        <button className="m-2 rounded bg-cyan-500  px-4 py-2 text-white">
+          Search
+        </button>
+      </div>
+
+      <div className="flex items-center justify-center container mx-auto">
+        <div className="grid gap-8 grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 p-4 pb-8 md:pb-20">
+          {data
+            .filter((item) => {
+              return searchQuery.toLocaleLowerCase() === ""
+                ? item
+                : item.continent
+                    .toLowerCase()
+                    .includes(searchQuery.toLowerCase());
+            })
+            .map(({ city, content }: dataType) => {
+              return <Card key={city} city={city} content={content} />;
+            })}
+        </div>
+      </div>
+    </>
+  );
+}
